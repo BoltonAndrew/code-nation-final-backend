b82d02092566396bd9c1efbfd45278ce80d8c848
Taha <mt.zngn@gmail>
2021-02-22 12:23:23 +0000

    movie model created

---
+++ src/models/Movies.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const moviesSchema = new mongoose.Schema({
+movie_id: {
+    type: String,
+    required: true,
+    unique: true,
+},
+movie_title: {
+    type: String,
+    required: true,
+},
+userLikes: [
+    {type: String}
+],
+userRates: [
+    {
+        userId : {type: String},
+        rate: {type: String}
+    }
+],
+}, {timestamps: true});
+
+const Movies = mongoose.model("Movies", moviesSchema)
+
+module.exports = {
+    Movies
+}
