5fca8201867107a6714481669fa5e36607060350
Taha <mt.zngn@gmail>
2021-02-22 10:39:13 +0000

    db connection made, server file created

---
+++ src/db/connection.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const connection = async () => {
+try {
+    await mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true,
+    useUnifiedTopology: true});
+    console.log("connected")
+
+} catch (error) {
+    console.log(error)
+}
+
+};
+
+connection();
