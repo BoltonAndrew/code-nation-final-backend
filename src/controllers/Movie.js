6f6c06eb3a15af4341f391971f5e5bc262b861da
Taha <mt.zngn@gmail>
2021-02-23 14:57:01 +0000

    movie routes for getMovie, getAllMovies, updateMovie, deleteMovie added

--- src/controllers/Movie.js
+++ src/controllers/Movie.js
@@ -15,3 +15,48 @@ exports.addMovie = async (req, res) => {
         }
     }
 };
+
+exports.getMovie = async (req, res) => {
+    try {
+        const movie = await Movie.find({movie_id: req.body.movie_id});
+        res.status(200).send(movie);
+    } catch (error) {
+        res.status(500).send({message: "Couldn not find the movie"});
+
+    }
+}
+
+exports.getAllMovies = async (req, res) => {
+    try {
+        const allMovies = await Movie.find({});
+        res.status(200).send(allMovies);
+
+    } catch (error) {
+        res.status(500).send({message: "Couldn load the movies"});
+
+    }
+}
+
+exports.updateMovie = async (req, res) => {
+    try {
+        
+        const query = {movie_id: req.body.movie_id}
+        const newMovie = req.body;
+        const updatedMovie = await Movie.findOneAndUpdate(query, newMovie, { new: true});
+        res.status(200).send({messgae: `movie succesfully updated !`})
+
+    } catch (error) {
+        res.status(500).send({message: "Couldn update the movie"});
+    }
+}
+
+exports.deleteMovie = async (req, res) => {
+    try {
+        const movie = await Movie.findOneAndDelete({movie_id: req.body.movie_id});
+        res.status(200).send({message: `Movie Deleted`})
+
+    } catch (error) {
+        res.status(500).send({message: "Movie couldnt deleted"});
+        console.log(error)
+    }
+}
--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,10 +1,15 @@
 const {Router} = require("express");
-const { addMovie } = require("../controllers/Movie")
+const { addMovie, getMovie, getAllMovies, updateMovie, deleteMovie } = require("../controllers/Movie")
 const movieRouter = Router();
 // const { auth } = require("../middleware/index")
 
-
+//CRUD movies
 movieRouter.post("/movie/add", addMovie);
+movieRouter.get("/movie", getMovie);
+movieRouter.get("/moviesAll", getAllMovies);
+movieRouter.patch("/movie", updateMovie);
+movieRouter.delete("/movie", deleteMovie);
+
 
 
 
